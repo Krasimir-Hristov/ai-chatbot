9b221fc7f398f2e203783e0f0ad30a6cd23faff4
krasi <krasimir.xristov@gmail.com>
2025-02-10 19:02:40 +0100

    feat: add animated chat icon with toggle functionality in Chat component

--- app/page.tsx
+++ app/page.tsx
@@ -62,6 +62,30 @@ export default function Chat() {
   return (
     <div className='flex flex-col min-h-screen'>
       <LandingSections />
+      <AnimatePresence>
+        {showChatIcon && (
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 100 }}
+            transition={{ duration: 0.2 }}
+            className='fixed bottom-4 right-4 z-50'
+          >
+            <Button
+              ref={chatIconRef}
+              onClick={toggleChat}
+              size='icon'
+              className='rounded-full size-14 p-2 shadow-lg'
+            >
+              {!isChatOpen ? (
+                <MessageCircle className='size-12' />
+              ) : (
+                <ArrowDownCircleIcon />
+              )}
+            </Button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
